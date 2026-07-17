973a525d8832fe1a6ef98982c4bf0f03834e65bf
savagerkin <erkin.uyguroglu@gmail.com>
2026-07-18 00:10:53 +0200

    food box sucks

--- scripts/food_box.gd
+++ scripts/food_box.gd
@@ -5,10 +5,9 @@ var game_manager : GameManager
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	game_manager = Constants.game_manager
-	collison.body_entered.connect(on_body_entered)
 
 
-func on_body_entered()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
