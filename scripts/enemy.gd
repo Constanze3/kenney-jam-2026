61f46c4fe2d92bc2935b1e5e389644d304f02dd9
savagerkin <erkin.uyguroglu@gmail.com>
2026-07-17 20:20:08 +0200

    feat: set up enemy spawner

---
+++ scripts/enemy.gd
@@ -0,0 +1,23 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := (Vector3(0,position.y,0) - position).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
---
+++ scripts/enemy_spawner.gd
@@ -0,0 +1,21 @@
+extends Node
+@export var enemy_scene : PackedScene
+@export var spawn_radius : float
+var angle : float
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	return
+	# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	_spawn_enemy()
+
+func _spawn_enemy() -> void:
+	var new_enemy := enemy_scene.instantiate() as Node3D
+	add_child(new_enemy) 
+	angle = randf() * TAU
+	new_enemy.global_position =  (Vector3 (
+			cos(angle) * spawn_radius, 
+			10.0, 
+			sin(angle) * spawn_radius 
+			))
+ 
